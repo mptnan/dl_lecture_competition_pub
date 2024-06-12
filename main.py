9d64bbe6ea7ee88530862757c5f4552c6bee928b
nishi <21977496+mptnan@users.noreply.github.com>
2024-06-12 21:06:13 +0900

    add num_workers argument

--- main.py
+++ main.py
@@ -425,6 +425,7 @@ def main(cfg: DictConfig):
     seed = cfg.env.seed
     num_epoch = cfg.env.num_epoch
     lr = cfg.env.lr
+    num_workers = cfg.env.num_workers
 
     hydra_output_dir = Path(hydra.core.hydra_config.HydraConfig.get().runtime.output_dir)
     logger.info(f"output into {hydra_output_dir}")
@@ -440,8 +441,18 @@ def main(cfg: DictConfig):
     test_dataset = VQADataset(df_path="./data/valid.json", image_dir="./data/valid", transform=transform, answer=False)
     test_dataset.update_dict(train_dataset)
 
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=128, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=False)
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=128,
+        shuffle=True,
+        num_workers=num_workers,
+    )
+    test_loader = torch.utils.data.DataLoader(
+        test_dataset,
+        batch_size=1,
+        shuffle=False,
+        num_workers=num_workers,
+    )
 
     model = VQAModel(vocab_size=len(train_dataset.question2idx) + 1, n_answer=len(train_dataset.answer2idx)).to(device)
 
