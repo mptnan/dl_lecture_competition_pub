077b3e986b636c2c3ce748234253f1e5cb2fd93d
nishi <21977496+mptnan@users.noreply.github.com>
2024-07-06 22:04:51 +0900

    separate source codes

---
+++ src/data.py
@@ -0,0 +1,151 @@
+import re
+from statistics import mode
+
+import numpy as np
+import pandas as pd
+import torch
+from PIL import Image
+
+
+def process_text(text):
+    """
+    sentence: str -> processed sentence: str
+    """
+    # lowercase
+    text = text.lower()
+
+    # 数詞を数字に変換
+    num_word_to_digit = {
+        "zero": "0",
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9",
+        "ten": "10",
+    }
+    for word, digit in num_word_to_digit.items():
+        text = text.replace(word, digit)
+
+    # 小数点のピリオドを削除
+    text = re.sub(r"(?<!\d)\.(?!\d)", "", text)
+
+    # 冠詞の削除
+    text = re.sub(r"\b(a|an|the)\b", "", text)
+
+    # 短縮形のカンマの追加
+    contractions = {
+        "dont": "don't",
+        "isnt": "isn't",
+        "arent": "aren't",
+        "wont": "won't",
+        "cant": "can't",
+        "wouldnt": "wouldn't",
+        "couldnt": "couldn't",
+    }
+    for contraction, correct in contractions.items():
+        text = text.replace(contraction, correct)
+
+    # 半角英数字、空白文字、'、:以外をスペースに変換（マルチバイト文字は？）
+    text = re.sub(r"[^\w\s':]", " ", text)
+
+    # 連続するスペースを1つに変換
+    text = re.sub(r"\s+", " ", text).strip()
+
+    return text
+
+
+# 1. データローダーの作成
+class VQADataset(torch.utils.data.Dataset):
+    def __init__(self, df_path, image_dir, transform=None, answer=True):
+        self.transform = transform  # 画像の前処理
+        self.image_dir = image_dir  # 画像ファイルのディレクトリ
+        self.df = pd.read_json(df_path)  # 画像ファイルのパス，question, answerを持つDataFrame
+        self.answer = answer
+
+        # question / answerの辞書を作成
+        self.question2idx = {}
+        self.answer2idx = {}
+        self.idx2question = {}
+        self.idx2answer = {}
+
+        # 質問文に含まれる単語を辞書に追加
+        for question in self.df["question"]:
+            question = process_text(question)
+            words = question.split(" ")
+            for word in words:
+                if word not in self.question2idx:
+                    self.question2idx[word] = len(self.question2idx)
+        self.idx2question = {v: k for k, v in self.question2idx.items()}  # 逆変換用の辞書(question)
+
+        if self.answer:
+            # 回答に含まれる単語を辞書に追加
+            for answers in self.df["answers"]:
+                for answer in answers:
+                    word = answer["answer"]
+                    word = process_text(word)
+                    if word not in self.answer2idx:
+                        self.answer2idx[word] = len(self.answer2idx)
+            self.idx2answer = {v: k for k, v in self.answer2idx.items()}  # 逆変換用の辞書(answer)
+
+    def update_dict(self, dataset):
+        """
+        検証用データ，テストデータの辞書を訓練データの辞書に更新する．
+
+        Parameters
+        ----------
+        dataset : Dataset
+            訓練データのDataset
+        """
+        self.question2idx = dataset.question2idx
+        self.answer2idx = dataset.answer2idx
+        self.idx2question = dataset.idx2question
+        self.idx2answer = dataset.idx2answer
+
+    def __getitem__(self, idx):
+        """
+        対応するidxのデータ（画像，質問，回答）を取得．
+
+        Parameters
+        ----------
+        idx : int
+            取得するデータのインデックス
+
+        Returns
+        -------
+        image : torch.Tensor  (C, H, W)
+            画像データ
+        question : torch.Tensor  (vocab_size)
+            質問文をone-hot表現に変換したもの
+        answers : torch.Tensor  (n_answer)
+            10人の回答者の回答のid
+        mode_answer_idx : torch.Tensor  (1)
+            10人の回答者の回答の中で最頻値の回答のid
+        """
+        image = Image.open(f"{self.image_dir}/{self.df['image'][idx]}")
+        image = self.transform(image)
+        question = np.zeros(len(self.idx2question) + 1)  # 未知語用の要素を追加
+        question_words = process_text(self.df["question"][idx]).split(" ")
+
+        # question: idxの質問文に含まれる単語に1それ以外に0の入ったベクトル
+        for word in question_words:
+            try:
+                question[self.question2idx[word]] = 1
+            except KeyError:
+                question[-1] = 1  # 未知語
+
+        if self.answer:
+            answers = [self.answer2idx[process_text(answer["answer"])] for answer in self.df["answers"][idx]]
+            mode_answer_idx = mode(answers)  # 最頻値を取得（正解ラベル）
+
+            return image, torch.Tensor(question), torch.Tensor(answers), int(mode_answer_idx)
+
+        else:
+            return image, torch.Tensor(question)
+
+    def __len__(self):
+        return len(self.df)
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,74 @@
+import multiprocessing
+import os
 import random
+from logging import DEBUG, INFO, Formatter, StreamHandler, getLogger
+
 import numpy as np
 import torch
+import yaml
 
-def set_seed(seed: int = 0) -> None:
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"  # mute tensorflow warnings
+import tensorflow as tf
+
+
+def set_seed(seed):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+
+def prepare_logger():
+    logger = getLogger("dl_lecture_main.py")
+    if logger.hasHandlers():
+        return logger
+
+    logger.setLevel(DEBUG)
+    h = StreamHandler()
+    h.setLevel(INFO)
+    f = Formatter("[%(levelname)s][%(asctime)s]: %(message)s")
+    h.setFormatter(f)
+    logger.addHandler(h)
+    return logger
+
+
+def device_info():
+    devices = {}
+
+    # tpu
+    devices["tpu"] = {}
+    try:
+        tpu = tf.distribute.cluster_resolver.TPUClusterResolver()  # TPU detection
+        devices["tpu"]["available"] = True
+        devices["tpu"]["num_accelerators"] = tpu.num_accelerators()["TPU"]
+    except ValueError:
+        devices["tpu"]["available"] = False
+
+    # gpu cuda
+    devices["cuda"] = {}
+    if torch.cuda.is_available():
+        devices["cuda"]["available"] = True
+        devices["cuda"]["device_count"] = torch.cuda.device_count()
+    else:
+        devices["cuda"]["available"] = False
+
+    # cpu
+    devices["cpu"] = {}
+    devices["cpu"]["available"] = True
+    devices["cpu"]["cpu_count"] = multiprocessing.cpu_count()
+
+    devices["info"] = "available devices:\n" + yaml.dump(devices)
+
+    return devices
+
+
+def get_yes_no(msg):
+    while True:
+        reply = str(input(msg + " (Y/n): ")).lower().strip()
+        if reply == "y":
+            return True
+        if reply == "n":
+            return False
