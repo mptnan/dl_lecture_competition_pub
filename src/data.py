b35935d42e3ba231ca03d1e54bf0ce48aed34705
nishi <21977496+mptnan@users.noreply.github.com>
2024-07-14 18:16:58 +0900

    fix global_mode_except_unanswerable_tensors

--- src/data.py
+++ src/data.py
@@ -398,7 +398,7 @@ def global_mode_except_unanswerable_tensors(train_json_path: str, aidx: AnswerIn
     answers = data["answers"]
     res = {}
     for k, ans_l in answers.items():
-        tmp = [aidx.str_to_idx[process_answer(ans["answer"])] for ans in ans_l if ans["answer"] != "unanswerable"]
+        tmp = [aidx.str_to_idx[process_answer(ans["answer"])] for ans in ans_l if process_answer(ans["answer"]) != "unanswerable"]
         if not tmp:
             tmp = [aidx.str_to_idx["unanswerable"]]
         res[k] = answer_indices_to_tensor(multimode(tmp), len(aidx))
