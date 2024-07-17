1c8c92f87e39067375de18361d96ca3c2b7d86dc
nishi <21977496+mptnan@users.noreply.github.com>
2024-07-17 14:58:29 +0900

    debug

--- main_bert_question_onehot_answer.py
+++ main_bert_question_onehot_answer.py
@@ -211,6 +211,9 @@ def main(cfg: DictConfig):
             for i in preds:
                 f.write(f"{i}\n")
         c = Counter(preds)
+        if epoch == 1:
+            print(preds)
+            print(c)
         _msg = "preds freq:\n" + "\n".join(
             [f"{idx}, {aidx.idx_to_str[idx]}: {freq}/{len(preds)}" for idx, freq in list(c.most_common())[:3]],
         )
