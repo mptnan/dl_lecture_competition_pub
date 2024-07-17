480717b0056a11b9570e58af3295305b788325be
nishi <21977496+mptnan@users.noreply.github.com>
2024-07-18 02:57:36 +0900

    fix

--- src/data.py
+++ src/data.py
@@ -640,7 +640,7 @@ class VQAStrQuestionOneHotAnswerDataset(torch.utils.data.Dataset):
         image = self.transform(image)
 
         if self.answer:
-            return image, self.questions[idx], self.answer_tensors[idx], torch.tensor(self.answers[idx]), idx
+            return image, self.questions[idx], self.answer_tensors[idx], self.answers[idx], idx
         else:
             return image, self.questions[idx]
 
